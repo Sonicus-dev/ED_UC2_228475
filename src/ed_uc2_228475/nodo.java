dc5fddcf941c0a00ff3a5cd304b26404644e8fae
Santiago Bojorquez Leyva <132846139+Sonicus-dev@users.noreply.github.com>
2025-03-09 19:37:34 -0700

    formateo de la clase nodo

--- src/ed_uc2_228475/nodo.java
+++ src/ed_uc2_228475/nodo.java
@@ -9,34 +9,28 @@ package ed_uc2_228475;
  * @author santi
  */
 public class nodo {
-    
+
     private Object dato;
 
-    
     private nodo siguiente;
 
-    
     public nodo(Object dato) {
         this.dato = dato;
         this.siguiente = null;
     }
 
-    
     public Object getDato() {
         return dato;
     }
 
-    
     public void setDato(Object dato) {
         this.dato = dato;
     }
 
-   
     public nodo getSiguiente() {
         return siguiente;
     }
 
-    
     public void setSiguiente(nodo siguiente) {
         this.siguiente = siguiente;
     }
