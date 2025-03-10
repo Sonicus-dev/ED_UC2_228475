497fe95af339f71890cef48ec367252a77c8f9d5
Santiago Bojorquez Leyva <132846139+Sonicus-dev@users.noreply.github.com>
2025-03-09 19:32:36 -0700

    creación de la clase nodo

---
+++ src/ed_uc2_228475/nodo.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed_uc2_228475;
+
+/**
+ *
+ * @author santi
+ */
+public class nodo {
+    
+    private Object dato;
+
+    
+    private nodo siguiente;
+
+    
+    public nodo(Object dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    
+    public Object getDato() {
+        return dato;
+    }
+
+    
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+   
+    public nodo getSiguiente() {
+        return siguiente;
+    }
+
+    
+    public void setSiguiente(nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
