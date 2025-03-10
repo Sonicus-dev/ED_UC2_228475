355d753a34798a149fa335786b9a146116e62e70
Santiago Bojorquez Leyva <132846139+Sonicus-dev@users.noreply.github.com>
2025-03-09 19:54:19 -0700

    Clase de pruebas

--- src/ed_uc2_228475/ED_UC2_228475.java
+++ src/ed_uc2_228475/ED_UC2_228475.java
@@ -14,7 +14,34 @@ public class ED_UC2_228475 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        listaEnlazadaSimple lista = new listaEnlazadaSimple();
+
+        lista.addFirst("Pastel");
+        lista.addFirst("Tarta");
+
+        lista.addLast("Pay");
+        lista.addLast("Cheesecake");
+
+        System.out.println("Lista antes de eliminar elementos:");
+        System.out.println(lista.toString());
+
+        lista.removeFirst();
+
+        System.out.println("Lista después de eliminar el primer elemento:");
+        System.out.println(lista.toString());
+
+        lista.removeLast();
+
+        System.out.println("Lista después de eliminar el último elemento:");
+        System.out.println(lista.toString());
+
+        System.out.println("¿La lista contiene 'Tarta'? " + lista.contains("Tarta"));
+
+        System.out.println("Elemento en la posición 1: " + lista.getElement(0));
+
+        lista.clear();
+
+        System.out.println("Lista después de eliminar todos los elementos:");
+        System.out.println(lista.toString());
     }
-    
 }
